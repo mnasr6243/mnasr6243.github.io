4f28a6d2618d0804b0d5f683e4fed7bc82eaee43
mnasr6243 <mnasr@csumb.edu>
2025-09-28 16:23:08 -0700

    Completed Q1-Q5 in scipt.js file. Q1 answers are randomized and all other questions have if else statements with the correct answers setting feedback to true and adding 20 to the score.

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -23,4 +23,54 @@ function loadQ1() {
 }
 loadQ1();
 
+// Handle quiz submition
+document.getElementById("quizForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    let score = 0;
+
+// Q1
+const q1Value = document.querySelector('input[name="q1"]:checked');
+if (q1Value && (q1Value.q1Value === "Fortnite" || q1Value.value === "Warzone")) {
+    score += 20;
+    showFeedback("fedback1", true);
+} else {
+    showFeedback("feedback1", false);
+}
+
+// Q2
+const q2 = [...document.querySelectorAll('input[name="q2"]:checked')].map(x => x.value);
+const correctQ2 = ["for", "while", "do-while"];
+if (arraysEqual(q2.sort(), correctQ2.sort())) {
+    score += 20;
+    showFeedback("feedback2", true);
+} else {
+    showFeedback("feedback2", false);
+}
+
+// Q3 
+const q3 = document.getElementById("q3").value;
+if (q3 === "script") {
+    score += 20;
+    showFeedback("feedback3", true);
+} else {
+    showFeedback("feedback3", false);
+}
+
+// Q4 
+const q4 = document.getElementById("q4").value;
+if (q4 === "10") {
+    score += 20;
+    showFeedback("feedback4", true);
+} else {
+    showFeedback("feedback4", false);
+}
+
+// Q5
+const q5 = document.getElementById("q5").value;
+if (q5 === "Hola" || q5 === "hola") {
+    score += 20;
+    showFeedback("feedback5", true);
+} else {
+    showFeedback("feedback5", false);
+}
 
