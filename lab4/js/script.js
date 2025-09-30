6295d148e8fce62ba9333883991563285c4cb17e
mnasr6243 <mnasr@csumb.edu>
2025-09-30 14:03:36 -0700

    Added event listeners, and completed setupStates function in script.js.

---
+++ lab4/js/script.js
@@ -0,0 +1,32 @@
+// Eventlisteners
+window.addEventListener("DOMContentLoaded", () => {
+    setupStates();
+    setupEvents();
+});
+
+// Fetch and display all US states from API
+async function setupStates() {
+    try {
+        let statesResponse = await fetch("https://csumb.space/api/allStatesAPI.php");
+        if (!statesResponse.ok) throw new Error("State API failed");
+
+        let statesData= await statesResponse.json();
+        let statesSelect = document.querySelector("#state");
+
+        // add placeholder
+        let defaultOpt = document.createElement("option");
+        defaultOpt.textContent = "Select State";
+        defaultOpt.disabled = true;
+        defaultOpt.selected = true;
+        statesSelect.appendChild(defaultOpt);
+
+        for (let s of statesData) {
+            let opt = document.createElement("option");
+            opt.value = s.usps;
+            opt.textContent = s.state;
+            statesSelect.appendChild(opt);
+        }
+    } catch (err) {
+        console.error("Error loading states:", err);
+    }       
+}
